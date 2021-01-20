1599ed5beee4a9e7dcee78802153d29e753d453c
iammi <iammi@DESKTOP-3FJ971H>
2021-01-20 15:27:27 -0800

    somon says ip

--- src/_02_simon_says/SimonSays.java
+++ src/_02_simon_says/SimonSays.java
@@ -51,21 +51,32 @@ public class SimonSays extends KeyAdapter {
 
 	public void keyPressed(KeyEvent e) {
 		// 15. Make a points variable to track the score.
+		int score = 0;
 
 		// 16. If the keyCode matches the imageIndex and "Simon says"
+		if(simonSays == true && e.getKeyCode() == imageIndex) {
 
 		// 17. Increase the value of score
+		score++;	
+		
 
 		// 18. Use the speak method to tell the user they were correct
+		speak("correct");
+		}
 
 		// 19. If the keyCode doesn't match the imageIndex and "Simon didn't
 		// say..."
+		if(simonSays == false && e.getKeyCode() != imageIndex) {
 
 		// 20. Increase the value of score
+			score++;
 
 		// 21. Use the speak method to tell the user they were correct
+			speak("correct");
+		}
 
 		// 22. Increment tries by 1
+		tries++;
 
 		// 25. If tries is greater than 9 (or however many you want)...
 
@@ -80,27 +91,42 @@ public class SimonSays extends KeyAdapter {
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
+		frame = new JFrame();
+		
 
 		// 6. Set the frame to visible
+		frame.setVisible(true);
 
 		// 7. Uncomment the following line to add a random image to your frame
-		// frame.add(getNextRandomImage());
+		frame.add(getNextRandomImage());
 
 		// 8. Set the name of your frame
 
 		// 9. Pack the frame
+		frame.pack();
 
 		// 10. Set the defaultCloseOperation of your frame to
 		// JFrame.EXIT_ON_CLOSE
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// 11. Add a key listener to the frame
+		frame.addKeyListener(this);
 
 		// 12. Create a new instance of Random
+		Random random = new Random();
 
 		// 13. Use the Random and the speak method to either say
 		// "Simon says press this key" or "Press this key"
+		simonSays = random.nextBoolean();
+		if(simonSays == false) {
+			speak("Press this key");
+		}
+		else {
+			speak("Simon says press this key");
+		}
 
 		// 14. Above, set the value of simonSays to true/false appropriately
+		
 
 	}
 
