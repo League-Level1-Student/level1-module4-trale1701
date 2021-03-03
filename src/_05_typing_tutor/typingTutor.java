7c5e4cd2b363867024625cb5837738efd122ee98
iammi <iammi@DESKTOP-3FJ971H>
2021-03-03 15:29:25 -0800

    typingTutor

--- src/_05_typing_tutor/typingTutor.java
+++ src/_05_typing_tutor/typingTutor.java
@@ -1,16 +1,22 @@
 package _05_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class typingTutor {
+public class typingTutor implements KeyListener {
 	
 	public char currentLetter;
-	
+	JLabel label = new JLabel();
+	JPanel panel = new JPanel();
 	public static void main(String[] args) {
+		typingTutor tutor = new typingTutor();
+		tutor.setup(); 
 		
 	}
 	
@@ -20,12 +26,47 @@ public class typingTutor {
 	}
 	
 	void setup(){
-         JFrame frame = new JFrame();
-         JPanel panel = new JPanel();
-         currentLetter = generateRandomLetter();
-         JLabel lable = new JLabel();
-         lable.setText(currentLetter);
-         
-         }
+		JFrame frame = new JFrame();
+        
+        currentLetter = generateRandomLetter();
+        
+        label.setFont(label.getFont().deriveFont(28.0f));
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setText("" + currentLetter);
+        frame.addKeyListener(this);
+        panel.add(label);
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+        }
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		if(arg0.getKeyChar()==currentLetter) {
+			System.out.println("correct");
+			panel.setBackground(Color.GREEN);
+			
+		}
+		else {
+			System.out.println("wrong");
+			panel.setBackground(Color.red);
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		currentLetter = generateRandomLetter();
+		label.setText("" + currentLetter);
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
