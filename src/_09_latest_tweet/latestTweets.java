f732b32d580d2af56f0709a42ebe31ea95f89b6b
iammi <iammi@DESKTOP-3FJ971H>
2021-04-14 15:37:18 -0700

    latest tweets

--- src/_09_latest_tweet/latestTweets.java
+++ src/_09_latest_tweet/latestTweets.java
@@ -1,5 +1,61 @@
 package _09_latest_tweet;
 
-public class latestTweets {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class latestTweets implements ActionListener {
+	JFrame frame = new JFrame("Latest Tweets");
+	JTextField field = new JTextField(20);
+	JButton button = new JButton("search");
+	JPanel panel = new JPanel();
+	
+	public void run() {
+		panel.add(field);
+		panel.add(button);
+		frame.add(panel);
+		button.addActionListener(this);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	private String getLatestTweet(String searchingFor) {
+
+	    Twitter twitter = new TwitterFactory().getInstance();
+
+	    AccessToken accessToken = new AccessToken(
+	        "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+	        "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+	    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+	        "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+	    twitter.setOAuthAccessToken(accessToken);
+
+	Query query = new Query(searchingFor);
+	    try {
+	        QueryResult result = twitter.search(query);
+	        return result.getTweets().get(0).getText();
+	    } catch (Exception e) {
+	        System.err.print(e.getMessage());
+	        return "What the heck is that?";
+	    }
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		String s = field.getText();
+		JOptionPane.showMessageDialog(null, getLatestTweet(s));
+	}
 
 }
---
+++ src/_09_latest_tweet/latestTweetsRunner.java
@@ -0,0 +1,9 @@
+package _09_latest_tweet;
+
+public class latestTweetsRunner {
+	public static void main(String[] args) {
+		latestTweets l = new latestTweets();
+		l.run();
+	}
+
+}
