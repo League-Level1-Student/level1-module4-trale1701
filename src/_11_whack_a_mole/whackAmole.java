18ba312edc9823e5d2e439da832f4a9eba61aabe
iammi <iammi@DESKTOP-3FJ971H>
2021-05-26 14:56:59 -0700

    whack a mole ip

--- src/_11_whack_a_mole/whackAmole.java
+++ src/_11_whack_a_mole/whackAmole.java
@@ -10,81 +10,15 @@ import javax.swing.JPanel;
 public class whackAmole implements ActionListener {
 		JFrame frame = new JFrame("Whack a button for flame and glory!!");
 		JButton button = new JButton();
-		JButton button2 = new JButton();
-		JButton button3 = new JButton();
-		JButton button4 = new JButton();
-		JButton button5 = new JButton();
-		JButton button6 = new JButton();
-		JButton button7 = new JButton();
-		JButton button8 = new JButton();
-		JButton button9 = new JButton();
-		JButton button10 = new JButton();
-		JButton button11 = new JButton();
-		JButton button12 = new JButton();
-		JButton button13 = new JButton();
-		JButton button14 = new JButton();
-		JButton button15 = new JButton();
-		JButton button16 = new JButton();
-		JButton button17 = new JButton();
-		JButton button18 = new JButton();
-		JButton button19 = new JButton();
-		JButton button20 = new JButton();
-		JButton button21 = new JButton();
-		JButton button22 = new JButton();
-		JButton button23 = new JButton();
-		JButton button24 = new JButton();
+		
 		JPanel panel = new JPanel();
 	
 	public void run() {
 		panel.add(button);
-		panel.add(button2);
-		panel.add(button3);
-		panel.add(button4);
-		panel.add(button5);
-		panel.add(button6);
-		panel.add(button7);
-		panel.add(button8);
-		panel.add(button9);
-		panel.add(button10);
-		panel.add(button11);
-		panel.add(button12);
-		panel.add(button13);
-		panel.add(button14);
-		panel.add(button15);
-		panel.add(button16);
-		panel.add(button17);
-		panel.add(button18);
-		panel.add(button19);
-		panel.add(button20);
-		panel.add(button21);
-		panel.add(button22);
-		panel.add(button23);
-		panel.add(button24);
+		
 		
 		button.addActionListener(this);
-		button2.addActionListener(this);
-		button3.addActionListener(this);
-		button4.addActionListener(this);
-		button5.addActionListener(this);
-		button6.addActionListener(this);
-		button7.addActionListener(this);
-		button8.addActionListener(this);
-		button9.addActionListener(this);
-		button10.addActionListener(this);
-		button11.addActionListener(this);
-		button12.addActionListener(this);
-		button13.addActionListener(this);
-		button14.addActionListener(this);
-		button15.addActionListener(this);
-		button16.addActionListener(this);
-		button17.addActionListener(this);
-		button18.addActionListener(this);
-		button19.addActionListener(this);
-		button20.addActionListener(this);
-		button21.addActionListener(this);
-		button22.addActionListener(this);
-		button23.addActionListener(this);
-		button24.addActionListener(this);
+		
 		
 		frame.add(panel);
 		frame.pack();
