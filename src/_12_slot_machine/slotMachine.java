186fc1725d29b92df52b2e938dc0168101e397bf
iammi <iammi@DESKTOP-3FJ971H>
2021-06-23 15:30:23 -0700

    slot machine ip

--- src/_12_slot_machine/slotMachine.java
+++ src/_12_slot_machine/slotMachine.java
@@ -1,5 +1,44 @@
 package _12_slot_machine;
 
-public class slotMachine {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class slotMachine implements ActionListener {
+	JFrame frame = new JFrame();
+	JLabel label1 = new JLabel();
+	JLabel label2 = new JLabel();
+	JLabel label3 = new JLabel();
+	JButton button = new JButton();
+	
+	
+	public void run(){
+		button.addActionListener(this);
+	}
+	
+	private JLabel createLabelImage(String fileName) throws MalformedURLException{
+        URL imageURL = getClass().getResource(fileName);
+	if (imageURL == null){
+		System.err.println("Could not find image " + fileName);
+		return new JLabel();
+	}
+	Icon icon = new ImageIcon(imageURL);
+	JLabel imageLabel = new JLabel(icon);
+	return imageLabel;
+}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
